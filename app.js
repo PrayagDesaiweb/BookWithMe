300550cc7986fa5597e4538593ce500eecea6da5
PrayagDesaiweb <prayagdesai24397@gmail.com>
2019-01-07 12:43:53 +0530

    compeleted index page. Issues with image rendering

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+// include from library
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+// express app running
+const app = express();
+
+// setting ejs templating engine
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.get('/',(req, res, next) =>{
+    res.render('non-registered/index',{
+        title:'Index page',
+        imageUrl:"views\non-registered\images\downtown.jpg"
+    });
+})
+
+app.listen(3000);
