916ac650410e96e6ad35e88d55c3169f7f52a07e
PrayagDesaiweb <prayagdesai24397@gmail.com>
2019-01-08 22:12:29 +0530

    issues regarding parsing of request bodies

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const bodyParser = require('body-parser');
 // express app running
 const app = express();
 
+
 // setting ejs templating engine
 app.set('view engine','ejs');
 app.set('views','views');
 
 // static folder to serve css images and js content
 app.use(express.static(path.join(__dirname, 'public')));
+
+// middlware for parsing body
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Importing Routers
 const nonRegisteredUsersRouter = require('./routers/non_registered_users');
--- controllers/admin_c.js
+++ controllers/admin_c.js
@@ -1,3 +1,4 @@
 exports.postbecomeHost = (req,res,next) =>{
+    console.log(req.body);
     res.send('I am handled');
 }
