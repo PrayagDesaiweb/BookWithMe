9fcdf89c1104549e923b46d530bb773be8db935b
PrayagDesaiweb <prayagdesai24397@gmail.com>
2019-01-17 11:10:51 +0530

    modified create-first-rental view by replacing checkboxes with radio buttons and changing the conflicting property names in the ejs view

--- controllers/registered-hosts.js
+++ controllers/registered-hosts.js
@@ -17,5 +17,14 @@ exports.postCreateOneRental = (req, res, next) => {
 
 exports.postFetchfromCreatefirstRentals = (req, res, next) => {
     console.log(req.body)
+    const host_name = req.body.host_name;
+    const host_id = req.body.host_id;
+    const property_name = req.body.property_name;
+    const address = req.body.address;
+    const description = req.body.description;
+    const chk_in_date = req.body.chk_in_date;
+    const chk_out_date = req.body.chk_out_date;
+
+
     res.send('I am handled');
 }
