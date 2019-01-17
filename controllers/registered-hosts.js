9f5ca3479663120687ea7d364c43b04659f0811d
PrayagDesaiweb <prayagdesai24397@gmail.com>
2019-01-17 11:35:50 +0530

    Error in register-host.js controller

--- controllers/registered-hosts.js
+++ controllers/registered-hosts.js
@@ -1,4 +1,5 @@
 const RegisterHost = require('../models/RegisterHost')
+const RegisterHostProperty = require('../models/RegisterHostProperty');
 
 exports.postCreateOneRental = (req, res, next) => {
     user_name = req.body.user_name;
@@ -20,10 +21,18 @@ exports.postFetchfromCreatefirstRentals = (req, res, next) => {
     const host_name = req.body.host_name;
     const host_id = req.body.host_id;
     const property_name = req.body.property_name;
+    const property_class = req.body.property_class;
     const address = req.body.address;
     const description = req.body.description;
     const chk_in_date = req.body.chk_in_date;
     const chk_out_date = req.body.chk_out_date;
+
+    const registerProperty = new RegisterHostProperty(host_name,host_id,property_name,property_class,address,description,chk_in_date,chk_out_date);
+    registerProperty.save().then(result =>{
+        console.log(result);
+    }).catch(err =>{
+        console.log(err)
+    })
 
 
     res.send('I am handled');
---
+++ models/RegisterHostProperty.js
@@ -0,0 +1,26 @@
+const mongoDb = require('mongodb');
+
+const getDb = require('../util/database').getDb;
+
+class RegisterHostProperty{
+    constructor(host_name, host_id, property_name, property_class,address, description,chk_in_date,chk_out_date){
+        this.host_name = host_name;
+        this.host_id = host_id;
+        this.property_name = property_name;
+        this.property_class = property_class;
+        this.address = address;
+        this.description = description;
+        this.chk_in_date = chk_in_date;
+        this.chk_out_date = chk_out_date;
+    }
+
+    save(){
+        let db = getDb();
+        return db.collection('hostProperty').insertOne(this)
+        .then(result =>{
+            console.log(result);
+        }).catch(err =>{
+            console.log(err);
+        });
+    }
+}
