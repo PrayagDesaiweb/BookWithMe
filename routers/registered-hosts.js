e15f314fa386010a6da9b320d163f54a5e7f61c0
PrayagDesaiweb <prayagdesai24397@gmail.com>
2019-03-07 15:10:26 +0530

    Error in /update-credentials in email field

--- routers/registered-hosts.js
+++ routers/registered-hosts.js
@@ -6,7 +6,7 @@ router.post('/create-first-rental',registeredHostsController.postCreateOneRental
 router.post('/path-to-manage-rentals',registeredHostsController.postFetchfromCreatefirstRentals);
 router.get('/create-rentals',registeredHostsController.getCreateRental);
 router.post('/create-rental',registeredHostsController.postCreateRental);
-router.get('/update-credentials',registeredHostsController.getUpdateCredentials);
+router.get('/update-credentials',registeredHostsController.getUpdateCredentials); // error here
 router.post('/updated-credentials',registeredHostsController.updateCredentials);
 router.post('/edit-rental',registeredHostsController.postEditRental);
 router.post('/delete-rental',registeredHostsController.postDeleteRental);
